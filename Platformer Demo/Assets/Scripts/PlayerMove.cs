7cc31b01159cc125feae344a51ca364ec4caa6c0
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2024-08-08 14:44:14 +1000

    Added in-air control
    Refactored & organised code

--- Platformer Demo/Assets/Scripts/PlayerMove.cs
+++ Platformer Demo/Assets/Scripts/PlayerMove.cs
@@ -5,9 +5,13 @@ using System.Data.Common;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float maxSpeed = 5f;
+#region Parameters
+    [SerializeField] private Vector2 maxSpeed = new Vector2(5f,5f);
     [SerializeField] private float speedWindow = 0.6f;
     [SerializeField] private float acceleration = 50f;
     [SerializeField] private float gravity = 33f;
@@ -15,18 +19,28 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private Transform rightRaycast;
     [SerializeField] private float raycastDist = 0.5f;
     [SerializeField] private LayerMask floorLayer;
+#endregion 
 
+#region Constants
     // a normal with y-part greater than 0.5f is a 60° incline or flatter
     private const float MIN_GROUND_NORMAL_Y = 0.5f;
+#endregion
 
+#region Actions
     // Actions
     private Actions actions;
     private InputAction moveAction;
     private InputAction jumpAction;
+#endregion
 
+#region Components
     // Components
     new private Rigidbody2D rigidbody;
     new private BoxCollider2D collider;
+#endregion
+
+#region State
+    // State
 
     private struct Contact 
     {
@@ -46,11 +60,14 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    // State
     private Vector2 movementDir;
     private List<Contact> contacts;
     private List<ContactPoint2D> contactPoints;
     private bool wasOnGround = false;
+
+    private float lastJumpPressedTime = float.NegativeInfinity;
+    private float lastOnGroundTime = float.NegativeInfinity;
+#endregion
 
 #region Init
     void Awake()
@@ -82,6 +99,9 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         movementDir = moveAction.ReadValue<Vector2>();
+        if (jumpAction.WasPerformedThisFrame()) {
+            lastJumpPressedTime = Time.time;
+        }
     }
 
 #endregion Update
@@ -94,10 +114,26 @@ public class PlayerMove : MonoBehaviour
 
         if (wasOnGround && !onGround) 
         {
+            // teleport to the ground it close enough
             onGround = TeleportToGround();
         }
 
-        float targetSpeed = maxSpeed * movementDir.x;
+        if (onGround) 
+        {
+            lastOnGroundTime = Time.fixedTime;
+            MoveOnGround();
+        }
+        else 
+        {
+            MoveInAir();
+        }
+
+        wasOnGround = onGround;
+    }
+
+    private void MoveOnGround() 
+    {
+        float targetSpeed = maxSpeed.x * movementDir.x;
         Contact? furthestContact = GetFurthestContact(targetSpeed);
 
         Vector2 dir = Vector2.right;
@@ -109,14 +145,28 @@ public class PlayerMove : MonoBehaviour
         }
 
         rigidbody.velocity = targetSpeed * dir;
+    }
 
-        if (!onGround) 
+    private void MoveInAir()
+    {
+        float targetSpeed = maxSpeed.x * movementDir.x;
+
+        // set horizontal velocity directly
+        Vector2 v = rigidbody.velocity;
+        v.x = targetSpeed;
+        
+        if (v.y > maxSpeed.y)
         {
-            rigidbody.AddForce(gravity * rigidbody.mass * Vector2.down);
+            // hit maximum speed, no gravity.
+            v.y = maxSpeed.y;
+        }
+        else {
+            // apply gravity
+            rigidbody.AddForce(gravity * rigidbody.mass * Vector2.down);           
         }
 
-        wasOnGround = onGround;
-    }
+        rigidbody.velocity = v;
+    } 
 
     private bool GroundContacts()
     {
